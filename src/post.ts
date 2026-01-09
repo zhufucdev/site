b7aa1de638de503c1d183c9ef7741eba6f43bfb5
zhufucdev <zhufuzhufu1@gmail.com>
2026-01-10 07:46:25 +0800

    :memo: update status

--- src/post.ts
+++ src/post.ts
@@ -1,11 +1,15 @@
+import type { ImageMetadata } from "astro";
 import type { SupportedLocale } from "./strings/types";
 
+interface Cover {
+  image: ImageMetadata;
+  alt: string;
+}
 export default interface Post {
   created: number;
   header: string;
   title: string;
   summary: string;
-  cover?: string;
+  cover?: Cover;
   mask: string;
 }
-
