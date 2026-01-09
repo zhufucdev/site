d1915f45afb70a7c27bc42bc88ec457e46b1cdb3
zhufucdev <zhufuzhufu1@gmail.com>
2026-01-09 21:27:05 +0800

    :sparkles: sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import { loadEnv } from "vite";
+
 import tailwindcss from "@tailwindcss/vite";
 
 import cloudflare from "@astrojs/cloudflare";
@@ -9,8 +11,17 @@ import solidJs from "@astrojs/solid-js";
 
 import mdx from "@astrojs/mdx";
 
+import sitemap from "@astrojs/sitemap";
+
+const { PUBLIC_SITE } = loadEnv(
+  process.env.NODE_ENV ?? "release",
+  process.cwd(),
+  "",
+);
+
 // https://astro.build/config
 export default defineConfig({
+  site: PUBLIC_SITE,
   vite: {
     plugins: [tailwindcss()],
   },
@@ -23,7 +34,6 @@ export default defineConfig({
   adapter: cloudflare({
     imageService: "compile",
   }),
-  integrations: [solidJs(), mdx()],
+  integrations: [solidJs(), mdx(), sitemap()],
 });
-
 
