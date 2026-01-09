41c8e63c751bd2cead247e019ac79054791fed99
zhufucdev <zhufuzhufu1@gmail.com>
2026-01-10 07:06:32 +0800

    :globe_with_meridans: add traditional chinese support

---
+++ src/pages/zh-tw/rss.xml.ts
@@ -0,0 +1,4 @@
+import getRssXmlApiRoute from "../../utils/rss-template";
+import zhTwRes from "../../strings/zh-tw";
+
+export const GET = getRssXmlApiRoute(zhTwRes);
---
+++ src/strings/zh-tw.ts
@@ -0,0 +1,24 @@
+import type { I18nResource } from "./types";
+
+const zhTwRes: I18nResource = {
+  localeName: "zh-tw",
+  all_articles_para: "所有文章",
+  highlights_para: "亮点",
+  about_me_para: "亮點",
+  read_full_para: "閱讀全文",
+  subsribe_via_rss_para: "透過 RSS 訂閱",
+  copy_link_para: "複製連結",
+  get_list_para: "取得文章列表",
+  get_source_code_para: "取得原碼",
+  license_para: "許可證",
+  reading_para: "正在讀",
+  playing_para: "正在玩",
+  socials_para: "社群媒體",
+  asterisk_bad_at_gaming_span: "*不懂玩遊戲",
+  via_span: (provider: string) => `透過${provider}`,
+  steve_reeds_blog_para: "Steve Reed的部落格",
+  oops_para: "哎呀，出錯了",
+  copy_failed_you_may_go_manual_para: "複製失敗，你可以手動複製以下內容",
+};
+
+export default zhTwRes;
