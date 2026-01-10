1dac1bfcb48dd1196450de661aced8537faca223
zhufucdev <zhufuzhufu1@gmail.com>
2026-01-10 09:35:08 +0800

    :bug: missing zh-tw in getI18nResByLocale

--- src/utils/i18n-res-by-locale.ts
+++ src/utils/i18n-res-by-locale.ts
@@ -1,6 +1,7 @@
 import type { I18nResource, SupportedLocale } from "../strings/types";
 import enRes from "../strings/en";
 import zhRes from "../strings/zh";
+import zhTwRes from "../strings/zh-tw";
 
 export default function getI18nResByLocale(
   name: SupportedLocale,
@@ -11,6 +12,7 @@ export default function getI18nResByLocale(
     case "zh":
       return zhRes;
     case "zh-tw":
+      return zhTwRes;
     default:
       return enRes;
   }
