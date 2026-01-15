8437b3d5800145109e614056c000b37f8a6010b7
zhufucdev <zhufuzhufu1@gmail.com>
2026-01-15 23:25:14 +0800

    :sparkles: PATCH gallery item

--- src/pages/api/gallery/[id].ts
+++ src/pages/api/gallery/[id].ts
@@ -3,17 +3,19 @@ import db from "../../../db/connection";
 import { galleryTable } from "../../../db/schema/gallery";
 import { imagesTable } from "../../../db/schema/images";
 import { eq } from "drizzle-orm";
+import verifyForm from "../../../utils/verify-form";
+import { supportedLocales } from "../../../strings/types";
 
 export const prerender = false;
 
 export const GET: APIRoute = async ({ params }) => {
   const { id: _id } = params;
-  if (typeof _id === "undefined") {
+  if (typeof _id !== "string") {
     return new Response("id is required", { status: 400 });
   }
   const id = Number.parseInt(_id);
   if (!Number.isInteger(id)) {
-    return new Response("Invalid id", { status: 400 });
+    return new Response("id must be an integer", { status: 400 });
   }
   const items = await db
     .select({
@@ -22,6 +24,7 @@ export const GET: APIRoute = async ({ params }) => {
       tweet: galleryTable.tweet,
       image: imagesTable.url,
       created: galleryTable.created,
+      trashed: galleryTable.trashed,
     })
     .from(galleryTable)
     .where(eq(galleryTable.id, id))
@@ -39,7 +42,7 @@ export const DELETE: APIRoute = async ({ params }) => {
   }
   const id = Number.parseInt(_id);
   if (!Number.isInteger(id)) {
-    return new Response("Invalid id", { status: 400 });
+    return new Response("id must be an integer", { status: 400 });
   }
   const deletedItems = await db
     .delete(galleryTable)
@@ -51,9 +54,72 @@ export const DELETE: APIRoute = async ({ params }) => {
   }
   const { imageId, ...deletedItem } = deletedItems[0];
   const images = await db
-    .select({ url: imagesTable.url })
+    .select({ url: imagesTable.url, alt: imagesTable.alt })
     .from(imagesTable)
     .where(eq(imagesTable.id, imageId));
-  return new Response(JSON.stringify({ image: images[0].url, ...deletedItem }));
+  return new Response(
+    JSON.stringify({
+      image: images[0].url,
+      alt: images[0].alt,
+      ...deletedItem,
+    }),
+  );
 };
 
+export const PATCH: APIRoute = async ({ params, request }) => {
+  const { id: _id } = params;
+  if (typeof _id !== "string") {
+    return new Response("id is required", { status: 400 });
+  }
+  const id = Number.parseInt(_id);
+  if (!Number.isInteger(id)) {
+    return new Response("id must be an integer", { status: 400 });
+  }
+  let _body;
+  try {
+    _body = JSON.parse(await request.text());
+  } catch (e) {
+    return new Response("Invalid body", { status: 400 });
+  }
+  const { locale, tweet, imageId, trashed } = _body;
+  try {
+    verifyForm(_body, {
+      ignoreUnknownKeys: true,
+      expectedTypes: {
+        locale: ["string", "undefined", "null"],
+        tweet: ["string", "undefined", "null"],
+        imageId: ["number", "undefined"],
+        trashed: ["boolean", "undefined"],
+      },
+    });
+  } catch (e) {
+    return new Response((e as Error).message, { status: 400 });
+  }
+  if (locale && !supportedLocales.includes(locale)) {
+    return new Response(
+      "locale must be one of " + supportedLocales.join(", "),
+      { status: 400 },
+    );
+  }
+  const modifiedItems = await db
+    .update(galleryTable)
+    .set({
+      locale,
+      tweet,
+      imageId,
+      trashed,
+    })
+    .where(eq(galleryTable.id, id))
+    .returning();
+  if (modifiedItems.length <= 0) {
+    return new Response("Item not found", { status: 404 });
+  }
+  const { imageId: currentImageId, ...modifiedItem } = modifiedItems[0];
+  const images = await db
+    .select({ url: imagesTable.url })
+    .from(imagesTable)
+    .where(eq(imagesTable.id, currentImageId));
+  return new Response(
+    JSON.stringify({ image: images[0].url, ...modifiedItem }),
+  );
+};
