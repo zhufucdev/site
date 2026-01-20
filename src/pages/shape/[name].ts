91c7ee3b33cea69bd6dafd7d9f7d2f3c374e10a4
zhufucdev <zhufuzhufu1@gmail.com>
2026-01-20 14:18:38 +0800

    :bug: shape prerender not working, switching to dynmaic

--- src/pages/shape/[name].ts
+++ src/pages/shape/[name].ts
@@ -1,12 +1,9 @@
-import type { APIRoute, GetStaticPaths } from "astro";
+import type { APIRoute } from "astro";
 import { shapeByName } from "../../shapes/by-name";
-import { supportedShapes } from "../../shapes/types";
 import type { SupportedShape } from "../../shapes/types";
 import { getImage } from "astro:assets";
 
-export const getStaticPaths = (() => {
-  return supportedShapes.map((name) => ({ params: { name } }));
-}) satisfies GetStaticPaths;
+export const prerender = false;
 
 export const GET: APIRoute = async ({ params, rewrite }) => {
   const { name } = params;
